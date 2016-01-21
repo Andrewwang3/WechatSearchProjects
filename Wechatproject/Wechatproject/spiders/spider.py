d36c082dd781f5c4edcd48f76df007149aec6fc1
lining0806 <lining0806@gmail.com>
2016-01-21 14:04:14 +0800

    test

--- Wechatproject/Wechatproject/spiders/spider.py
+++ Wechatproject/Wechatproject/spiders/spider.py
@@ -2,9 +2,6 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import Selector
 from Wechatproject.items import WechatprojectItem
-# import sys
-# sys.path.append("..")
-# from items import WechatprojectItem
 from bs4 import BeautifulSoup
 from scrapy.http import Request
 
@@ -18,25 +15,26 @@ class WechatSpider(BaseSpider):
     querystring = u"清华"
     type = 2 # 2-文章，1-微信号
     for i in range(1, 50, 1):
-        start_urls.append("http://weixin.sogou.com/weixin?query=%s&type=%d&page=%d&ie=utf8" % (querystring, type, i))
+        start_urls.append("http://weixin.sogou.com/weixin?type=%d&query=%s&page=%d" % (type, querystring, i))
     # print start_urls
 
     #############################################################################################
     ## 递归抓取
 
-    # ## 使用xpath()方法，注意item中键对值为string类型，extract()方法返回list
-    # def parse(self, response):
-    #     # print response.body
-    #     sel = Selector(response)
-    #     sites = sel.xpath('//div[@class="txt-box"]/h4/a')
-    #     for site in sites:
-    #         item = WechatprojectItem()
-    #         item["title"] = site.xpath("text()").extract() # 其中在item.py中定义了title = Field()
-    #         item["link"] = site.xpath("@href").extract() # 其中在item.py中定义了link = Field()
-    #         #############################################################################################
-    #         # yield item ## 只抓取当前页数据
-    #         # yield Request(url=item["link"][0], callback=self.parse2) ## 只抓取二级页面数据
-    #         yield Request(url=item["link"][0], meta={"item":item}, callback=self.parse2) ## 抓取当前页数和二级页面数据
+    ## 使用xpath()方法，注意item中键对值为string类型，extract()方法返回list
+    def parse(self, response):
+        # print response.body
+        sel = Selector(response)
+        sites = sel.xpath('//div[@class="txt-box"]/h4/a')
+        for site in sites:
+            item = WechatprojectItem()
+            item["title"] = site.xpath("text()").extract() # 其中在item.py中定义了title = Field()
+            item["link"] = site.xpath("@href").extract() # 其中在item.py中定义了link = Field()
+            #############################################################################################
+            # yield item ## 只抓取当前页数据
+            next_url = item["link"][0]
+            # yield Request(url=next_url, callback=self.parse2) ## 只抓取二级页面数据
+            yield Request(url=next_url, meta={"item":item}, callback=self.parse2) ## 抓取当前页数和二级页面数据
 
     ## 使用BeautifulSoup方法，注意item中键对值为string类型
     def parse(self, response):
@@ -49,8 +47,9 @@ class WechatSpider(BaseSpider):
             item["link"] = tag.find("a").get("href") # 其中在item.py中定义了link = Field()
             #############################################################################################
             # yield item ## 只抓取当前页数据
-            # yield Request(url=item["link"], callback=self.parse2) ## 只抓取二级页面数据
-            yield Request(url=item["link"], meta={"item":item}, callback=self.parse2) ## 抓取当前页数和二级页面数据
+            next_url = item["link"]
+            # yield Request(url=next_url, callback=self.parse2) ## 只抓取二级页面数据
+            yield Request(url=next_url, meta={"item":item}, callback=self.parse2) ## 抓取当前页数和二级页面数据
 
     def parse2(self, response):
         soup = BeautifulSoup(response.body)
